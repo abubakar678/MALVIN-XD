00d33bd38a80b35babd313ca36ebdfee006cc6b1
abubakar678 <138264686+abubakar678@users.noreply.github.com>
2025-08-17 14:29:03 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~pi1H3ZqB#6rbaJffrbJshn-F5gF7_oOhogX_n6Olt_9VN6JxZLqI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "03244294603", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
